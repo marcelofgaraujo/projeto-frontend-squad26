86f7c17c649219be5a2d28d763a920b01c158b7f
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-04-09 19:05:25 -0300

    estilização da tela 01

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import Home from './pages/Home'
+import '/src/styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,44 @@
 import { useState } from 'react'
+import '/src/styles/index.css'
 
 function Home() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <>
+      <header>
+        <div className='logo' />
+        <div className='avatar' />
       </header>
-    </div>
+      <main>
+        <aside>
+          <div className='main-image' />
+          <form method='GET'>
+          <input type={'search'} placeholder='O que você procura?' />
+          <button type='submit'></button>
+          </form>
+          <section className='suggestion'>
+          <strong>Sugestões:</strong>
+          <div>Acessibilidade</div>
+          <div>React</div>
+          <div>Scrum</div>
+          </section>
+        </aside>
+        <article>
+          <h3>Como funciona o Technical Share?</h3>
+          <ul>
+            <div className='hiddenBar' />
+            <div className="list">Pesquise por um colaborador ou assunto de interesse</div>
+            <div className="list">Escolha o perfil mais indicado para lhe ajudar</div>
+            <div className="list">Faça login para enviar mensagem ao colaborador</div>
+            <div className="list">Envie mensagem com suas dúvidas</div>
+            <div className="list">Pronto, agora é só aguardar o retorno do colaborador escolhido</div>
+          </ul>
+        </article>
+      </main>
+      <footer>
+        <div className='logo' />
+      </footer>
+    </>
   )
 }
 
