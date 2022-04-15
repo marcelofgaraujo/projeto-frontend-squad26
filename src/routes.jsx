8d2ee70e00c94e2503eb19f111518a3128738141
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-04-15 19:23:25 -0300

    ajustes na rota e nos estilos

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes as Server, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
-import Search from "./pages/Search-page/Search"
 import Login from "./pages/loginPage/Login"
 import Chat from "./pages/chat/Chat"
 import SearchAcessibilidade from './pages/Search-page/searchs-estáticos/search-acessibilidade'
